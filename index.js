27474e95795f6ad001d61269f497c36619fcfc6b
Ara Yeressian <ayeressian2@gmail.com>
2015-09-25 16:55:12 +0400

    change get to post

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 let app = require('express')(),
     shell = require('shelljs');
 
-app.get('/', (req, res) => {
+app.post('/', (req, res) => {
     console.log('hit');
     shell.exec('./pull-create.sh testDocker ayeressian/testdocker https://github.com/bazillionbeings/testdocker.git');
     res.end();
