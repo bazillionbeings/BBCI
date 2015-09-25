c180debed80ba4692fc955e2fc00bb746bbe3905
Ara Yeressian <ayeressian2@gmail.com>
2015-09-25 16:47:51 +0400

    testing

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let app = require('express')(),
     shell = require('shelljs');
 
 app.get('/', (req, res) => {
+    console.log('hit');
     shell.exec('./pull-create.sh testDocker ayeressian/testdocker https://github.com/bazillionbeings/testdocker.git');
     res.end();
 });
